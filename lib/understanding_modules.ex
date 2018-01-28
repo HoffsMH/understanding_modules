3197bee2c89c784cf675ca69d6517722d14daab2
HoffsMH <hecker.matthew@gmail.com>
2018-01-28 17:11:05 -0500

    didnt know about this defdelegate

--- lib/understanding_modules.ex
+++ lib/understanding_modules.ex
@@ -18,6 +18,18 @@ defmodule UnderstandingModules do
 
       iex> UnderstandingModules.alias_wrapper_three
       :function_from_alias
+
+      iex> UnderstandingModules.import_wrapper_from_toplevel
+      :import
+
+      iex> UnderstandingModules.Nested.nestedFunction
+      :this_is_nested
+
+      iex> UnderstandingModules.some_other_split "hi"
+      ["hi"]
+
+      iex> UnderstandingModules.split "hi"
+      ["hi"]
   """
 
   # this wont execute
@@ -33,10 +45,18 @@ defmodule UnderstandingModules do
 
   alias SomeNamespace.AliasMeTo, as: SomethingElse
 
-  
   use UseMe
-  
+
+  # this makes the functions available in this module but not from without
+  # and its still not possible to call something like
+  # UnderstandingModules.ImportMe.function_from_import
   import ImportMe
+
+  # This does not error even though this file isn't anywhere
+  alias Aalkdflaksjdfllkjasdf
+
+  # This WILL error however
+  # import Asldkjasldfkjasf
 
   #notice that both of these work
   def top_level_alias_wrapper do
@@ -62,4 +82,21 @@ defmodule UnderstandingModules do
     alias SomeNamespace.AliasMeTo, as: Ok
     Ok.function_from_alias
   end
+
+  # this still doesnt get us to accessing this function from the outside
+  def import_wrapper_from_toplevel do
+    function_from_import()
+  end
+
+  defmodule Nested do
+    def nestedFunction do
+      :this_is_nested
+    end
+  end
+
+  # looks like we still need to specify arity when using this method
+  defdelegate some_other_split(asdf), to: String.Break, as: :split
+
+  # basically this is a public "import"
+  defdelegate split(asdf), to: String.Break
 end
