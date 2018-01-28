e3e5925323ad7355534ddbc2d0f76ab0ef1f9e45
HoffsMH <hecker.matthew@gmail.com>
2018-01-28 16:10:29 -0500

    paired down doctest to figure out what gets it to execute... spoiler its just iex> in the attribute

---
+++ lib/some_other_namespace/alias_me_too.ex
@@ -0,0 +1,5 @@
+defmodule SomeNamespace.AliasMeTo do
+  def function_from_alias do
+    :function_from_alias
+  end
+end
--- lib/understanding_modules.ex
+++ lib/understanding_modules.ex
@@ -3,23 +3,27 @@ defmodule UnderstandingModules do
   Documentation for UnderstandingModules.
   """
 
-  @doc ~S"""
-  Parses the given `line` into a command.
-
-  ## Examples
-
+  @doc """
       iex> UnderstandingModules.top_level_alias_wrapper
       :toplevel_alias
 
       iex> UnderstandingModules.top_level_alias_wrapper_with_as
       :toplevel_alias
 
+      iex> UnderstandingModules.alias_wrapper
+      :function_from_alias
+
+      iex> UnderstandingModules.alias_wrapper_two
+      :function_from_alias
   """
 
   # this basically does nothing
   alias AliasMe
 
   alias AliasMe, as: Taco
+
+  alias SomeNamespace.AliasMeTo, as: SomethingElse
+
   
   use UseMe
   
@@ -31,5 +35,16 @@ defmodule UnderstandingModules do
   end
   def top_level_alias_wrapper_with_as do
     Taco.function_from_toplevel_alias
+  end
+
+  # notice that this works even though the directory names aren't
+  # matching. It appears that everything is available
+
+  def alias_wrapper do
+    SomeNamespace.AliasMeTo.function_from_alias
+  end
+
+  def alias_wrapper_two do
+    SomethingElse.function_from_alias
   end
 end
